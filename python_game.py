d18be511a38ba6859aac89c63107621f816df0bf
Frederik Junk <frederik_junk@t-online.de>
2023-04-15 22:22:24 +0200

    know the blockers are in the leakedBoard

--- python_game.py
+++ python_game.py
@@ -284,39 +284,46 @@ def addPlacementBlocker(board, positionTupelList):
     blockerList = []
     
 
-
     if blockerTopNumber < 0 and blockerLeftNumber < 0 or board[blockerTopNumber][blockerLeftNumber] == 1:
         pass
     else:
         blockerList.append(blockerTopLeft = (blockerTopNumber, blockerLeftNumber))
+        board[blockerTopNumber][blockerLeftNumber] = 6
     if blockerTopNumber < 0 or board[blockerTopNumber][columnNumber] == 1:
         pass
     else:
         blockerList.append(blockerTop = (blockerTopNumber, columnNumber))
+        board[blockerTopNumber][columnNumber] = 6
     if blockerTopNumber < 0 and blockerRightNumber > 10 or board[blockerTopNumber][blockerRightNumber] == 0:
         pass
     else:
         blockerList.append(blockerTopRight = (blockerTopNumber, blockerRightNumber))
+        board[blockerTopNumber][blockerRightNumber] = 6
     if  blockerRightNumber > 10 or board[rowNumber][blockerRightNumber] == 1:
         pass
     else:
         blockerList.append(blockerRight = (rowNumber, blockerRightNumber))
+        board[rowNumber][blockerRightNumber] = 6
     if blockerBottomNumber > 10 and blockerRightNumber > 10 or board[blockerBottomNumber][blockerRightNumber] == 1:
         pass
     else:
         blockerList.append(blockerBottomRight = (blockerBottomNumber, blockerRightNumber))
+        board[blockerBottomNumber][blockerRightNumber] = 6
     if blockerBottomNumber > 10 or board[blockerBottomNumber][columnNumber] == 1:
         pass
     else:
         blockerList.append(blockerBottom = (blockerBottomNumber, columnNumber))
+        board[blockerBottomNumber][columnNumber] = 6
     if blockerBottomNumber > 10 and blockerLeftNumber < 0 or board[blockerBottomNumber][blockerLeftNumber] == 1:
         pass
     else:
         blockerList.append(blockerBottomLeft = (blockerBottomNumber, blockerLeftNumber))
+        board[blockerBottomNumber][blockerLeftNumber] = 6
     if blockerLeftNumber < 0 or board[rowNumber][blockerLeftNumber] == 1:
         pass
     else:
         blockerList.append(blockerLeft = (rowNumber, blockerLeftNumber))
+        board[rowNumber][blockerLeftNumber] = 6
 
 
 
