a6098bffbac374745d4d946923954fcae7f6a763
Dennis <Dennis.Schmitt1@gmx.de>
2023-04-16 00:57:57 +0200

    Updated test classes with new tests

--- test_oponent.py
+++ test_oponent.py
@@ -19,7 +19,7 @@ class TestOpponent(unittest.TestCase):
                             ]
         self.hiddenBoard = [
                             [0,0,0,0,0,0,0,0,0,0],
-                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,1,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,2,0,0],
                             [0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,1,0,0,0,0],
@@ -29,6 +29,8 @@ class TestOpponent(unittest.TestCase):
                             [0,0,0,0,0,0,0,0,0,0],
                             [0,0,0,0,0,0,0,0,0,0]
                             ]
+        row = 2
+        column = 4
 
     def test_opponentGetPosition(self):
         self.assertNotEqual(self.oschlachtschiff.getPosition(),0)
@@ -36,6 +38,9 @@ class TestOpponent(unittest.TestCase):
     def test_checkHit(self):
         self.assertEqual(checkHit(self.hiddenBoard, self.leakedBoard,1,1),1)
         self.assertEqual(checkHit(self.hiddenBoard, self.leakedBoard, 5,6),0)
+
+    # def test_opponentAction(self):
+    #     self.assertEqual(opponentAction(),1)
 
     def tearDown(self):
         del self.oschlachtschiff
