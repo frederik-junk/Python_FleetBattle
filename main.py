78e4742b575ef27b1b4da406609273450bf3b8c8
Anton Wolf <schulischeinfos@gmail.com>
2023-04-15 22:56:02 +0200

    added endBattle

--- main.py
+++ main.py
@@ -36,23 +36,24 @@ currentPlayer = 0
 
 # Function to select the game mode. Prints message if input is not 1 or 2 and calls itself.
 def gameModeSelection():
-    userInput = input("Bitte wähle die gewünschte Spielart: \n [1] Spiel gegen den Computer \n [2] 2 Spieler\n")
-    match userInput:
-        case "1":
+    gameMode = int(input("Bitte wähle die gewünschte Spielart: \n [1] Spiel gegen den Computer \n [2] 2 Spieler\n"))
+    match gameMode:
+        case 1:
             print("__________________________________\n")
             print("1-Spieler Modus.\n")
             print(gameRules)
-            userName1 = input("Bitte geben Sie ihren Namen an: \n")
-            user1.setName(userName1)
-            print (f"Hallo {user1.getName()}, bitte platzieren Sie nun Ihre Schiffe")
+            user1.setName("Der Computer")
+            userName2 = input("Bitte geben Sie ihren Namen an: \n")
+            user2.setName(userName2)
+            print (f"Hallo {user2.getName()}, bitte platzieren Sie nun Ihre Schiffe")
             placeShip(leakedBoard1, shipLength)
             print("Der Computer plaziert nun seine Schiffe. Dies kann einige Sekunden dauern")
-            cpuPlaceShip(leakedBoard2, shipLength)
+            #cpuPlaceShip(leakedBoard2, shipLength)
             print("")
             print("Das Spiel beginnt.")
-            user2.setName("Computer")
-            selectStartingPlayer(userInput, userName1)
-        case "2":
+            selectStartingPlayer(gameMode)
+            return gameMode
+        case 2:
             print("__________________________________\n")
             print("2-Spieler Modus.\n")
             print(gameRules)
@@ -67,7 +68,8 @@ def gameModeSelection():
             print ("!!WICHTIG!! Der andere Spieler sollte diesen Vorgang nicht sehen, bitte weggucken!")
             placeShip(leakedBoard2, shipLength)
             print("Das Spiel beginnt.")
-            selectStartingPlayer(userInput)
+            selectStartingPlayer(gameMode)
+            return gameMode
         case _: 
             print("Ungültige Eingabe.\n")
             gameModeSelection()
@@ -79,19 +81,19 @@ def selectStartingPlayer(mode):
     playerTwo = 2
     print("Es wird zufällig bestimmt wer beginnt...")
     startingPlayer = randint(1,2)
-    if(startingPlayer == 1):
-        currentPlayer = 1
-        print(f"{user1.getName()} darf das Spiel beginnen und ist an der Reihe")
-        return currentPlayer
-    elif(startingPlayer == 2):
+    if(startingPlayer == 2):
+        currentPlayer = 2
+        print(f"{user2.getName()} darf das Spiel beginnen und ist an der Reihe!")
+        #return currentPlayer
+    elif(startingPlayer == 1):
         if(mode == "1"):
-            print(f"{user2.getName()}ccccccc darf das Spiel beginnen und ist an der Reihe") #cccc ist als Kontrolltext mit drin um zu zeigen, dass hier der Computer spielt
+            print(f"{user1.getName()}ccccccc darf das Spiel beginnen und ist an der Reihe!") #cccc ist als Kontrolltext mit drin um zu zeigen, dass hier der Computer spielt
             currentPlayer = 2
-            return currentPlayer
+            #return currentPlayer
         else:
-            print(f"{user2.getName()} darf das Spiel beginnen und ist an der Reihe")
+            print(f"{user1.getName()} darf das Spiel beginnen und ist an der Reihe!")
             currentPlayer = 2
-            return currentPlayer
+            #return currentPlayer
 
 
 # Switches the current player after each action
@@ -105,10 +107,32 @@ def nextPlayer():
         print(f"{user1.getName()} ist nun an der Reihe.")
     return currentPlayer
 
+def battleEnd(winID, gameMode):
+    if winID == 1:
+        if gameMode == 1: #computer wins the game (1 Player mode)
+            loseoutput()
+            print(f"{user1.getName()} hat das Spiel gewonnen. {user2.getName()} versuche es doch noch einmal!")
+        else:
+            winoutput() #player 1 wins the game (2 Player mode)
+            print(f"Herzlichen Glueckwunsch {user1.getName()} du hast das Spiel gegen {user2.getName()} gewonnen!")
+    else:
+        if gameMode == 1: #player wins the game (1 Player mode)
+            winoutput()
+            print(f"Herzlichen Glueckwunsch {user2.getName()} du hast das Spiel gegen den Computer gewonnen!")
+        else:
+            winoutput() #player 2 wins the game (2 Player mode) mode
+            print(f"Herzlichen Glueckwunsch {user2.getName()} du hast das Spiel gegen {user1.getName()} gewonnen!")
+
+
+
 # Holds the logic of the game. Welcomes the user, asks for game mode selection and navigates through the game
 def main():
     welcomeUser()
-    gameModeSelection()
+    gameMode = gameModeSelection()
+    print (gameMode)
+    battleEnd(2, gameMode)
+
+    
 
 if __name__ == "__main__":
     main()
