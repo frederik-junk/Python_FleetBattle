24ccee952672ea3577bb7cc2eff5be098c1f4a01
Frederik Junk <frederik_junk@t-online.de>
2023-04-16 16:30:55 +0200

    fixed something

--- circularImportFixing.py
+++ circularImportFixing.py
@@ -52,4 +52,5 @@ oUboot4 = Uboot(emptyTupelList)
 opponentShips = [oSchlachtschiff1, oKreuzer1, oKreuzer2, oZertsoerer1, oZerstoerer2, oZerstoerer3, oUboot1, oUboot2, oUboot3, oUboot4]
 
 
+
 pKreuzer1.classPlaceShip(leakedBoard3, pKreuzer1)
