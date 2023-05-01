52a67c59195cf1ea44797fef0e806ae484a6cf34
Dennis <Dennis.Schmitt1@gmx.de>
2023-05-01 12:08:03 +0200

    Further work on tests

--- test_python_game.py
+++ test_python_game.py
@@ -1,52 +1,51 @@
 import unittest
-from unittest.mock import patch
-from io import StringIO
 import python_game
+from unittest.mock import MagicMock, patch
+from io import StringIO
+from contextlib import redirect_stdout
+import sys
+from converterfunctions import splitColumnConverter, splitRow
+
+#pylint: disable=C
 
 class TestPythonGame(unittest.TestCase):
 
     def setUp(self):
-        self.leakedBoard1 = []
-        for i in range(10):
-            row = []
-            for j in range(10):
-                row.append(0)
-            self.leakedBoard1.append(row)
+        self.board = ([[0]*10]*10)
 
-        self.leakedBoard2 = []
-        for x in range(10):
-            row = []
-            for y in range(10):
-                row.append(0)
-            self.leakedBoard2.append(row)
+    def test_splitColumnConverter(self):
+        self.assertEqual(splitColumnConverter("A1"), 0)
+        self.assertEqual(splitColumnConverter("b2"), 1)
+        self.assertEqual(splitColumnConverter("J3"), 9)
+        self.assertEqual(splitColumnConverter("Z1"), 11)
 
-    def test_place_ship(self):
-        with patch('builtins.input', side_effect=['A1', 'down']):
-            python_game.placeShip(self.leakedBoard1, 3, [1,1,1], 'TestShip', 1)
-        self.assertEqual(self.leakedBoard1[0][0], 1)
-        # self.assertEqual(self.leakedBoard[1][0], 1)
-        # self.assertEqual(self.leakedBoard[2][0], 1)
+    def test_splitRow(self):
+        self.assertEqual(splitRow("A1"), 0)
+        self.assertEqual(splitRow("b2"), 1)
+        self.assertEqual(splitRow("J3"), 2)
+        self.assertEqual(splitRow("11"), 0)
 
-    # def test_ship_direction(self):
-    #     self.leakedBoard[0][0] = 1
-    #     with patch('builtins.input', side_effect=['down']):
-    #         self.assertTrue(python_game.shipDirection(self.leakedBoard, 3, 0, 0, [1,1,1]))
+    def test_initializeBoard(self):
+        
+        python_game.initializeBoard = MagicMock()
+        python_game.initializeBoard.return_value = ([[0]*10]*10)
+        result = python_game.initializeBoard(self.board)
+        self.assertEqual(self.board, result)
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_print_leaked_board(self, mock_stdout):
-        self.leakedBoard1[0][0] = 1
-        self.printleakedBoard(self.leakedBoard1)
-        self.assertIn('A B C D E F G H I J', mock_stdout.getvalue())
+    # def test_printleakedBoard(self):
+    #     python_game.printleakedBoard = MagicMock()
+    #     expected_output = "  \\\\ A B C D E F G H I J\n01  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n02  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n03  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n04  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n05  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n06  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n07  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n08  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n09  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n10  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n"
+    #     result = python_game.initializeBoard(self.board)
+    #     with StringIO() as buf, redirect_stdout(buf):
+    #         python_game.printleakedBoard(self.board)
+    #         self.assertEqual(result, expected_output)
 
-    # @patch('sys.stdout', new_callable=StringIO)
-    # def test_print_hidden_board(self, mock_stdout):
-    #     self.hiddenBoard[0][0] = 1
-    #     self.printhiddenBoard(self.hiddenBoard)
-    #     self.assertIn('A B C D E F G H I J', mock_stdout.getvalue())
-
-    def tearDown(self):
-        del self.leakedBoard1
-        del self.hiddenBoard1
+    # def test_printhiddenBoard(self):
+    #     python_game.printhiddenBoard = MagicMock()
+    #     expected_output = "  \\\\ A B C D E F G H I J\n01  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n02  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n03  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n04  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n05  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n06  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n07  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n08  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n09  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n10  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n"
+    #     with StringIO() as buf, redirect_stdout(buf):
+    #         self.printhiddenBoard(self.board)
+    #         self.assertEqual(buf.getvalue(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
