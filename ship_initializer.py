7ec967aad4b3e1ff9944176c32fe27fc3759749f
Dennis <Dennis.Schmitt1@gmx.de>
2023-05-03 13:48:29 +0200

    Further renaming of modules / methods / functions

--- ship_initializer.py
+++ ship_initializer.py
@@ -5,37 +5,37 @@ empty_tupel_list = []
 VARIABLE = 0
 
 # these are the user1 ship player or cpu
-pSchlachtschiff1 = shipmanager.Schlachtschiff(empty_tupel_list, VARIABLE)
+pSchlachtschiff1 = ship_manager.Schlachtschiff(empty_tupel_list, VARIABLE)
 
-pKreuzer1 = shipmanager.Kreuzer(empty_tupel_list, VARIABLE)
-pKreuzer2 = shipmanager.Kreuzer(empty_tupel_list, VARIABLE)
+pKreuzer1 = ship_manager.Kreuzer(empty_tupel_list, VARIABLE)
+pKreuzer2 = ship_manager.Kreuzer(empty_tupel_list, VARIABLE)
 
-pZerstoerer1 = shipmanager.Zerstoerer(empty_tupel_list, VARIABLE)
-pZerstoerer2 = shipmanager.Zerstoerer(empty_tupel_list, VARIABLE)
-pZerstoerer3 = shipmanager.Zerstoerer(empty_tupel_list, VARIABLE)
+pZerstoerer1 = ship_manager.Zerstoerer(empty_tupel_list, VARIABLE)
+pZerstoerer2 = ship_manager.Zerstoerer(empty_tupel_list, VARIABLE)
+pZerstoerer3 = ship_manager.Zerstoerer(empty_tupel_list, VARIABLE)
 
-pUboot1 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
-pUboot2 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
-pUboot3 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
-pUboot4 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
+pUboot1 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
+pUboot2 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
+pUboot3 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
+pUboot4 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
 
 # playerShips = [pSchlachtschiff1, pKreuzer1, pKreuzer2, pZerstoerer1, pZerstoerer2, pZerstoerer3, pUboot1, pUboot2, pUboot3, pUboot4]
 playerShips = [pSchlachtschiff1]
 
 # these are are the user2 ships player1 or player2
-oSchlachtschiff1 = shipmanager.Schlachtschiff(empty_tupel_list, VARIABLE)
+oSchlachtschiff1 = ship_manager.Schlachtschiff(empty_tupel_list, VARIABLE)
 
-oKreuzer1 = shipmanager.Kreuzer(empty_tupel_list, VARIABLE)
-oKreuzer2 = shipmanager.Kreuzer(empty_tupel_list, VARIABLE)
+oKreuzer1 = ship_manager.Kreuzer(empty_tupel_list, VARIABLE)
+oKreuzer2 = ship_manager.Kreuzer(empty_tupel_list, VARIABLE)
 
-oZerstoerer1 = shipmanager.Zerstoerer(empty_tupel_list, VARIABLE)
-oZerstoerer2 = shipmanager.Zerstoerer(empty_tupel_list, VARIABLE)
-oZerstoerer3 = shipmanager.Zerstoerer(empty_tupel_list, VARIABLE)
+oZerstoerer1 = ship_manager.Zerstoerer(empty_tupel_list, VARIABLE)
+oZerstoerer2 = ship_manager.Zerstoerer(empty_tupel_list, VARIABLE)
+oZerstoerer3 = ship_manager.Zerstoerer(empty_tupel_list, VARIABLE)
 
-oUboot1 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
-oUboot2 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
-oUboot3 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
-oUboot4 = shipmanager.Uboot(empty_tupel_list, VARIABLE)
+oUboot1 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
+oUboot2 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
+oUboot3 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
+oUboot4 = ship_manager.Uboot(empty_tupel_list, VARIABLE)
 
 # opponentShips = [oSchlachtschiff1, oKreuzer1, oKreuzer2, oZerstoerer1, oZerstoerer2, oZerstoerer3, oUboot1, oUboot2, oUboot3, oUboot4]
 
